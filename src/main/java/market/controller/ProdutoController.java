a3da1b97ab816520d7b8af2894c8b5aba13bcf26
Vinicius <als.vinicius@gmail.com>
2019-12-18 22:05:07 -0300

    ADD: alteração e exclusão de produtos

--- src/main/java/market/controller/ProdutoController.java
+++ src/main/java/market/controller/ProdutoController.java
@@ -3,10 +3,13 @@ package market.controller;
 import market.model.Produto;
 import market.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/produtos")
@@ -22,6 +25,28 @@ public class ProdutoController {
     ) {
         produtoRepository.insert(produto);
         return produto;
+    }
+
+    @PatchMapping("/alterar/{id}")
+    @ResponseBody
+    public Optional<Produto> alterar(
+            @PathVariable Long id,
+            @RequestBody Produto produto
+    ) {
+        produto.setIdProduto(id);
+        produtoRepository.save(produto);
+        return produtoRepository.findById(id);
+    }
+
+    @DeleteMapping("/excluir/{id}")
+    @ResponseBody
+    public ResponseEntity excluir(
+            @PathVariable Long id
+    ) {
+        Produto produto = new Produto();
+        produto.setIdProduto(id);
+        produtoRepository.delete(produto);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping
